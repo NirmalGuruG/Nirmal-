1301565ce94351208aba6ff0778503bb368bc10b
NirmalGuruG <nirmalphillaur@gmail.com>
2025-10-04 20:36:30 +0530

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,12 @@
+first_name = "Nirmal"
+... last_name = "Guru"
+...
+... # Concatenate
+... full_name = first_name + " " + last_name
+...
+... # Display greeting
+... print("Hello,", full_name + "! Welcome to the Python world.")
+...
+Hello, Nirmal Guru! Welcome to the Python world.
+
+
